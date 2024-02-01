9d11a467715a45e1e7b571cbc37c6e4582d20e8c
arvin0099 <arvincxzc@gmail.com>
2024-01-31 20:34:05 -0800

    start

---
+++ models/contacts.js
@@ -0,0 +1,22 @@
+const contacts = [
+    {
+        firstName: 'Brandon',
+        lastName: 'Brewer',
+        cellPhone: 5353442355,
+        email: 'yoyo@yo.com',
+    },
+    {
+        firstName: 'Arvin',
+        lastName: 'Tanglao',
+        cellPhone: 5353442333,
+        email: 'yoyo@yo.coms',
+    },
+    {
+        firstName: 'Neil',
+        lastName: 'Mellstrom',
+        cellPhone: 5354523325,
+        email: 'yoyo@yo.com',
+    },
+]
+
+module.exports = contacts
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const contact = require('./models/contacts.js')
 const port = 3000
 const methodOverride = require('method-override')
 
